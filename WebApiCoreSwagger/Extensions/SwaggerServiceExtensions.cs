e0c935494305d65d26d6334d8c2fad7334518ab1
Ahd Ben Kheder <ahd.benkheder@vistaprint.com>
2018-08-10 18:17:46 +0100

    add bearer authorization token support

--- WebApiCoreSwagger/Extensions/SwaggerServiceExtensions.cs
+++ WebApiCoreSwagger/Extensions/SwaggerServiceExtensions.cs
@@ -1,17 +1,20 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Swashbuckle.AspNetCore.Swagger;
+using static WebApiCoreSwagger.Startup ;
 
 namespace WebApiCoreSwagger.Extensions
 {
     public static class SwaggerServiceExtensions
     {
-        #region Swagger
-        public static IServiceCollection AddSwaggerDoc(this IServiceCollection services)
+ 
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
         {
             services.AddSwaggerGen(option =>
             {
-                option.SwaggerDoc("v1.0", new Info { Title = "Discount Engine API", Version = "v1.0" });
+                option.SwaggerDoc("v1.0", new Info { Title = Configuration["API:APIName"],Version = Configuration["API:APIVersion"] });
 
             });
 
@@ -23,38 +26,37 @@ namespace WebApiCoreSwagger.Extensions
             app.UseSwagger();
             app.UseSwaggerUI(option =>
             {
-                option.SwaggerEndpoint("/swagger/v1.0/swagger.json", "v1.0");
+                option.SwaggerEndpoint("/swagger/v1.0/swagger.json", Configuration["API:APIVersion"]);
             });
 
             return app;
         }
 
-        #endregion Swagger
-
-        #region Swagger With Barber token
         public static IServiceCollection AddSwaggerWithBearerToken(this IServiceCollection services)
         {
-            services.AddSwaggerGen(option =>
+            services.AddSwaggerGen(c =>
             {
-                option.SwaggerDoc("v1.0", new Info { Title = "You API Name", Version = "v1.0" });
+                c.SwaggerDoc("v1.0", new Info { Title = Configuration["API:APIName"], Version = Configuration["API:APIVersion"] });
 
+                // Swagger 2.+ support
+                var security = new Dictionary<string, IEnumerable<string>>
+                {
+                    {"Bearer", new string[] { }},
+                };
+
+                c.AddSecurityDefinition("Bearer", new ApiKeyScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = "header",
+                    Type = "apiKey"
+                });
+                c.AddSecurityRequirement(security);
             });
 
             return services;
+
         }
 
-        public static IApplicationBuilder UseSwaggerWithBearerToken(this IApplicationBuilder app)
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI(option =>
-            {
-                option.SwaggerEndpoint("/swagger/v1.0/swagger.json", "v1.0");
-                option.RoutePrefix = string.Empty;
-            });
-
-            return app;
-        }
-
-        #endregion Swagger With Barber token
     }
 }
